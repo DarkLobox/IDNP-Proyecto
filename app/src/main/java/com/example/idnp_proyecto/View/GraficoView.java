b16eba8cb0d07f884be99f1728107fa11dc53c89
DarkLobox <57413922+DarkLobox@users.noreply.github.com>
2021-12-24 12:27:49 -0500

    Grafico de barras, ListAdapter para invitado y botones desahabilitados

---
+++ app/src/main/java/com/example/idnp_proyecto/View/GraficoView.java
@@ -0,0 +1,45 @@
+package com.example.idnp_proyecto.View;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.idnp_proyecto.R;
+import com.example.idnp_proyecto.ViewBarChart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraficoView extends Fragment {
+    View rootview ;
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.grafico_fragment, container, false);
+
+        List<String> horizontal = new ArrayList<String>();
+        horizontal.add("Peru");
+        horizontal.add("Argentina");
+        horizontal.add("Brasil");
+        horizontal.add("EEUU");
+        horizontal.add("Japon");
+
+        List<Integer> vertical = new ArrayList<Integer>();
+        vertical.add(60);
+        vertical.add(20);
+        vertical.add(65);
+        vertical.add(40);
+        vertical.add(90);
+
+        ViewBarChart grafico = rootview.findViewById(R.id.grafico);
+        grafico.setDatos(horizontal,vertical);
+
+        return rootview;
+    }
+
+
+}
